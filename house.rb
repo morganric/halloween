5fa7023dc523c3f9587298be19ae8119d21d8e80
morganric <rich@embedtree.com>
2012-10-10 20:15:00 +0100

    updated house and player classes / Models

---
+++ house.rb
@@ -0,0 +1,22 @@
+
+class House
+	# declare house attributes
+	attr_accessor :lightson
+	attr_accessor :athome
+	attr_accessor :trick_or_treat
+	attr_accessor :new_sweets
+	
+	# initialize house attributes
+	def initialize(lightson, athome, trick_or_treat, new_sweets)
+		@lights = lightson
+		@athome = athome
+		@trick_or_treat = trick_or_treat
+		@new_sweets = new_sweets
+		
+	
+	end
+	
+	
+	
+end
+	
